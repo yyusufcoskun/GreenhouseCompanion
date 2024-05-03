921b7a883de2cf7f6fb7cd9257d7acb79c7dc5d5
tohli <yusuf.coskun1@boun.edu.tr>
2024-05-03 21:56:08 +0300

    Small bug fix - model parameters were incorrect.

--- app/src/main/java/com/example/greenhousecompanion/api/model/LightModel.kt
+++ app/src/main/java/com/example/greenhousecompanion/api/model/LightModel.kt
@@ -1,5 +1,5 @@
 package com.example.greenhousecompanion.api.model
 
 data class LightIntensityData(
-    val humidity: Int
+    val lightIntensity: Int
 )
--- app/src/main/java/com/example/greenhousecompanion/api/model/SoilMoistureModel.kt
+++ app/src/main/java/com/example/greenhousecompanion/api/model/SoilMoistureModel.kt
@@ -1,5 +1,5 @@
 package com.example.greenhousecompanion.api.model
 
 data class SoilMoistureData(
-    val humidity: Double
+    val soilMoisture: Double
 )
